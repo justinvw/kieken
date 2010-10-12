271ccfb183a913e808399f959e3c1bcb60feee33
Justin van Wees <justin@vwees.net>
2010-10-12 10:17:19 +0200

    Changed db schema

--- kieken_db_schema.sql
+++ kieken_db_schema.sql
@@ -1,75 +1,57 @@
--- -----------------------------------------------------
--- Table `kieken_pictures`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `kieken_pictures` ;
-CREATE  TABLE IF NOT EXISTS `kieken_pictures` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT ,
-  `title` VARCHAR(255) NULL ,
-  `description` TEXT NULL ,
-  `license` SMALLINT NULL ,
-  `user_id` INT(11) NULL ,
-  `slug` VARCHAR(255) NOT NULL ,
-  `status` TINYINT(1) NOT NULL DEFAULT 0 ,
-  `updated` DATETIME NOT NULL ,
-  `created` DATETIME NOT NULL ,
-  PRIMARY KEY (`id`) ,
-  UNIQUE INDEX `slug_UNIQUE` (`slug` ASC) )
-ENGINE = MyISAM
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_unicode_ci;
+DROP TABLE IF EXISTS `kieken_albums`;
+CREATE TABLE IF NOT EXISTS `kieken_albums` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `parent_id` int(11) DEFAULT NULL,
+  `title` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `excerpt` tinytext COLLATE utf8_unicode_ci,
+  `description` text COLLATE utf8_unicode_ci,
+  `thumbnail_picture_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `slug` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `lft` int(11) DEFAULT NULL,
+  `rght` int(11) DEFAULT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT '0',
+  `updated` datetime NOT NULL,
+  `created` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `slug_UNIQUE` (`slug`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+-- --------------------------------------------------------
 
--- -----------------------------------------------------
--- Table `kieken_albums`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `kieken_albums` ;
-CREATE  TABLE IF NOT EXISTS `kieken_albums` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT ,
-  `parent_id` INT(11) NULL ,
-  `title` VARCHAR(255) NULL ,
-  `excerpt` TINYTEXT NULL ,
-  `description` TEXT NULL ,
-  `thumbnail_picture_id` INT(11) NULL ,
-  `user_id` INT(11) NULL ,
-  `slug` VARCHAR(255) NOT NULL ,
-  `lft` INT(11) NULL ,
-  `rght` INT(11) NULL ,
-  `status` TINYINT(1) NOT NULL DEFAULT 0 ,
-  `updated` DATETIME NOT NULL ,
-  `created` DATETIME NOT NULL ,
-  PRIMARY KEY (`id`) ,
-  UNIQUE INDEX `slug_UNIQUE` (`slug` ASC) )
-ENGINE = MyISAM
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_unicode_ci;
+DROP TABLE IF EXISTS `kieken_albums_pictures`;
+CREATE TABLE IF NOT EXISTS `kieken_albums_pictures` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `album_id` int(11) NOT NULL,
+  `picture_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=103 ;
+-- --------------------------------------------------------
 
--- -----------------------------------------------------
--- Table `kieken_albums_pictures`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `kieken_albums_pictures` ;
-CREATE  TABLE IF NOT EXISTS `kieken_albums_pictures` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT ,
-  `album_id` INT(11) NOT NULL ,
-  `picture_id` INT(11) NOT NULL ,
-  PRIMARY KEY (`id`) )
-ENGINE = MyISAM
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_unicode_ci;
+DROP TABLE IF EXISTS `kieken_files`;
+CREATE TABLE IF NOT EXISTS `kieken_files` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `picture_id` int(11) NOT NULL,
+  `width` int(6) NOT NULL,
+  `height` int(6) NOT NULL,
+  `filename` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `thumbname` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `updated` datetime NOT NULL,
+  `created` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `filename_UNIQUE` (`filename`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+-- --------------------------------------------------------
 
--- -----------------------------------------------------
--- Table `kieken_files`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `kieken_files` ;
-CREATE  TABLE IF NOT EXISTS `kieken_files` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT ,
-  `picture_id` INT(11) NOT NULL ,
-  `width` INT(6) NOT NULL ,
-  `height` INT(6) NOT NULL ,
-  `filename` VARCHAR(100) NULL ,
-  `filetype` VARCHAR(20) NOT NULL ,
-  `updated` DATETIME NOT NULL ,
-  `created` DATETIME NOT NULL ,
-  PRIMARY KEY (`id`) ,
-  UNIQUE INDEX `filename_UNIQUE` (`filename` ASC) )
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_unicode_ci;
+DROP TABLE IF EXISTS `kieken_pictures`;
+CREATE TABLE IF NOT EXISTS `kieken_pictures` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `description` text COLLATE utf8_unicode_ci,
+  `license` smallint(6) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT '0',
+  `updated` datetime NOT NULL,
+  `created` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+-- --------------------------------------------------------
